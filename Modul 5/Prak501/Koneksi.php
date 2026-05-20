e42193555883fbcc8131fa3450ac5898cc31dc95
Ahmad Luthfi Maulana <192307842+anomaliemas@users.noreply.github.com>
2026-05-20 19:08:42 +0800

    Update Koneksi.php

--- Modul 5/Prak501/Koneksi.php
+++ Modul 5/Prak501/Koneksi.php
@@ -1,7 +1,7 @@
 <?php
 function koneksi() {
     try {
-        $pdo = new PDO('mysql:host=bt2ibfusmhlviamfvibw-mysql.services.clever-cloud.com;port=3306;dbname=bt2ibfusmhlviamfvibw', 'usyjrm71k3r3a49t', '');
+        $pdo = new PDO('mysql:host=bt2ibfusmhlviamfvibw-mysql.services.clever-cloud.com;port=3306;dbname=bt2ibfusmhlviamfvibw', 'usyjrm71k3r3a49t', 'P2xREazmh3t0c9TCloYv');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
